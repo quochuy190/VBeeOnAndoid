2b4b4f230a21cf4fc490846892beef1567e10948
HuyNQ <Huynq52@vinsmart.net.vn>
2021-01-07 17:56:37 +0700

    update DAO tr

--- app/src/main/java/com/vn/vbeeon/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/vn/vbeeon/presentation/viewmodel/MainViewModel.kt
@@ -1,10 +1,12 @@
 package com.vn.vbeeon.presentation.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import com.vn.vbeeon.data.local.entity.DeviceEntity
 import com.vn.vbeeon.data.local.entity.UserEntity
 import com.vn.vbeeon.data.repository.DeviceRepository
 import com.vn.vbeeon.data.repository.UserRepository
 import com.vn.vbeeon.domain.model.Device
+import com.vn.vbeeon.domain.model.convertFromEntityList
 import com.vn.vbeeon.presentation.base.BaseViewModel
 import timber.log.Timber
 import javax.inject.Inject
@@ -16,8 +18,7 @@ class MainViewModel @Inject constructor() : BaseViewModel() {
     @Inject
     lateinit var repoDevice : DeviceRepository
 
-
-    public val devicesRes : MutableLiveData<List<Device>> = MutableLiveData()
+    val devicesRes : MutableLiveData<List<Device>> = MutableLiveData()
 
     init {
         Timber.e("init")
@@ -30,8 +31,14 @@ class MainViewModel @Inject constructor() : BaseViewModel() {
         error.value = throwable
     }
     fun loadDevices(){
-        repoDevice.getALL()
-        userRepository.createUserTest(UserEntity(1, "abc", "abc"))
-       // devicesRes.postValue(convertFromEntityList(repoDevice.getALL()))
+        repoDevice.insertDevice(DeviceEntity(0, "abc", 0, 0, "cdg"))
+//        repoDevice.getALL()
+//        userRepository.createUserTest(UserEntity(1, "abc", "abc"))
+        Timber.e("here ${repoDevice.getALL().size}")
+        var listDevice : MutableList<Device> = mutableListOf()
+        repoDevice.getALL().forEach {
+            listDevice.add(Device(it.id, it.name, it.intSource, it.categoryID, it.categoryName))
+        }
+        devicesRes.postValue(listDevice)
     }
 }
